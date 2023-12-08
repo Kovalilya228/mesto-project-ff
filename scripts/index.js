a32fca860339e33adbe1a5d8e5faa460a2e7b04f
Kovalilya228 <ilya.koval.2002@yandex.ru>
2023-12-08 17:04:20 +0300

    Added cards auto-fill and delete-function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function addCard(title, image, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = title;
+    cardElement.querySelector('.card__image').src = image;
+    cardElement.querySelector('.card__image').alt = title;
+    deleteButton.addEventListener('click', deleteCard);
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach( item => {
+    placesList.append(addCard(item.name, item.link, deleteCard));
+})
