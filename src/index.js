39bb5cae5760422cd0109d137bb46eb9e207644b
Kovalilya228 <ilya.koval.2002@yandex.ru>
2023-12-29 17:06:43 +0300

    Added webpack, loaders and plugins for js html css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import {initialCards} from './scripts/cards.js';
+import './pages/index.css';
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
